911351e64e9a7a8f69d1c2deba26cde827289443
ednilson19 <127148436+ednilson19@users.noreply.github.com>
2025-03-26 14:18:48 +0000

    overwrite

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,6 @@
-// Angular main entry point
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
