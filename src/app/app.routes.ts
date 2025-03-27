a02f5e9139b7dbeca1eaba51a659412837391fee
Iulian Ciobanu <liukciobanu@gmail.com>
2025-03-27 10:20:25 +0000

    Home, About Us + RouterModules

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AddexpenseComponent } from './addexpense/addexpense.component';
 import { MyexpensesComponent } from './myexpenses/myexpenses.component';
 import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
 export const routes: Routes = [
     {path:"addexpense", component: AddexpenseComponent},
     {path: "", component: HomeComponent},
-    {path: "myexpenses", component: MyexpensesComponent}
+    {path: "myexpenses", component: MyexpensesComponent},
+    {path: "aboutus", component: AboutusComponent}
 ];
--- src/app/myexpenses/myexpenses.component.ts
+++ src/app/myexpenses/myexpenses.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-myexpenses',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './myexpenses.component.html',
   styleUrl: './myexpenses.component.css'
 })
