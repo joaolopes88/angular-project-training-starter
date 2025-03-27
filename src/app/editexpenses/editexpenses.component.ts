f1b51e0f46a630af2903e0be4834420f2a0ead85
Iulian Ciobanu <liukciobanu@gmail.com>
2025-03-27 16:56:28 +0000

    +editexpenses (por remover depois apparently)

--- src/app/editexpenses/editexpenses.component.ts
+++ src/app/editexpenses/editexpenses.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-editexpenses',
-  imports: [],
+  imports: [RouterModule, NavbarComponent],
   templateUrl: './editexpenses.component.html',
   styleUrl: './editexpenses.component.css'
 })
