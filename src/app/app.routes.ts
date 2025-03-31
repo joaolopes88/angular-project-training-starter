60af228390148898bb49912f2c7fa0d207102035
ednilson19 <127148436+ednilson19@users.noreply.github.com>
2025-03-31 14:56:39 +0100

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { AddexpenseComponent } from './addexpense/addexpense.component';
 import { MyexpensesComponent } from './myexpenses/myexpenses.component';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { EditexpensesComponent } from './editexpenses/editexpenses.component';
+
 
 export const routes: Routes = [
     {path:"addexpense", component: AddexpenseComponent},
     {path: "", component: HomeComponent},
     {path: "myexpenses", component: MyexpensesComponent},
     {path: "dashboard", component: DashboardComponent},
-    {path: "editexpenses", component: EditexpensesComponent}
+ 
 ];
